2f8061e4ad8801c8214f2064863859df84ae6455
Derik Nascimento <derikwnacimento@gmail.com>
2023-05-03 20:28:45 -0300

    fix

--- scripts.js
+++ scripts.js
@@ -2,25 +2,24 @@ const botao = document.getElementById('mostrar-formulario');
 const formulario = document.getElementById('formulario');
 const botaoEnviar = document.getElementById('blt-enviar');
 let linhas = '';
+let numeroDeTelefone = []
 
-function fechaJanela () {
-const conteiner = document.getElementById('conteiner')
-conteiner.addEventListener('click', function(e){
-  if (!formulario.contains(e.target) && !e.target.matches('.conteiner')){
-      formulario.style.display = 'nome'
-  }
-})
+function FechaPopUp() {
+  document.addEventListener('click', function (e) {
+    if (!formulario.contains(e.target)) {
+      formulario.style.display = 'none'
+    }
+  })
 
 }
-
 botao.addEventListener('click', function () {
   formulario.style.display = 'block';
 });
 
 formulario.addEventListener("submit", function (e) {
   e.preventDefault();
+  FechaPopUp()
   Crialinhas();
-  fechaJanela()
   AtualizaTabela();
 });
 
@@ -30,19 +29,29 @@ function Crialinhas() {
 
   let linha = '<tr>';
   linha += `<td>${inputNome.value}</td>`;
-  linha += `<td>${inputTele.value}</td>`;
+  linha += `<td>${inputTele.value}</td>`
   linha += '</tr>';
 
   linhas += linha;
 
   inputNome.value = '';
   inputTele.value = '';
+
 }
+
+
+
 
 function AtualizaTabela() {
   const CorpoTabela = document.querySelector('tbody')
   CorpoTabela.innerHTML = linhas;
 }
+
+
+
+
+
+
 
 
 
